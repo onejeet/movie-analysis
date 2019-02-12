f27a9f2f8d88879de09bca949bafa8a1be371780
onejeet <jitendra909@gmail.com>
2019-02-12 15:15:38 +0530

    k formatted the budget

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -2,6 +2,23 @@ import React,{ Component } from 'react';
 //import { Link } from 'react-router-dom';
 
 class MovieItem extends Component {
+    numberFormatter = (num) => {
+        let formattedNum;
+        switch (true) {
+            case (num > 999999999):
+                formattedNum = (num/1000000000).toFixed(1) + 'b';
+                break;
+            case (num > 999999):
+                formattedNum = (num/1000000).toFixed(1) + 'm';
+                break;
+            case (num > 9999):
+                formattedNum = (num/1000).toFixed(1) + 'k';
+                break;
+            default:
+                formattedNum = num.toLocaleString();
+        }
+        return formattedNum;
+    }
 
     render(){
         const {movie} = this.props;
@@ -15,7 +32,7 @@ class MovieItem extends Component {
                     <td>{movie.language}</td>
                     <td>{movie.country}</td>
                     <td>{movie.rating}</td>
-                    <td>{movie.budget ? "$"+parseInt(movie.budget).toLocaleString() : " "}</td>
+                    <td>{movie.budget ? "$"+this.numberFormatter(parseInt(movie.budget)).toLocaleString() : " "}</td>
                     <td>{movie.year}</td>
                     <td><a href={movie.imdb_link} target="_blank" rel="noopener noreferrer"> IMDB Page <i className="fa fa-link" aria-hidden="true"></i> </a></td>
                 </tr>
