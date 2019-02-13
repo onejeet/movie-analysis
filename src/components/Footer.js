cb8823301134546dfc65f14f610437f1b3270dd7
onejeet <jitendra909@gmail.com>
2019-02-13 10:30:59 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+
+class Footer extends Component {
+
+    render(){
+        return (
+        <footer>
+            <div>
+                <i className="fa fa-code" aria-hidden="true"></i> code by <a href="https://www.github.com/onejeet" target="_blank" rel="noopener noreferrer" >Jitendra Sharma</a>
+            </div>
+        </footer>
+        );
+
+    }
+}
+
+export default Footer;
